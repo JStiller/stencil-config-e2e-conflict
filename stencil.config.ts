48a087015a6be63ae5ad1a996a6cf1c1ca1afeee
José Stiller <JStiller@users.noreply.github.com>
2021-11-14 22:30:08 +0100

    Initial setup bby using npm init stencil

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-config-e2e-conflict',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
